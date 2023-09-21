41a9cecc2fc009cf5b5b66aa927bf32f3f2bc10c
eneasmesquita <juniormesquitacunha@gmail.com>
2023-09-21 12:53:03 -0400

    all it's fine now

--- src/main/java/one/digitalinnovation/gof/facade/Facade.java
+++ src/main/java/one/digitalinnovation/gof/facade/Facade.java
@@ -1,8 +1,5 @@
 package one.digitalinnovation.gof.facade;
 
-import subsistema1.crm.CrmService;
-import subsistema2.cep.CepApi;
-
 public class Facade {
 
 	public void migrarCliente(String nome, String cep) {
